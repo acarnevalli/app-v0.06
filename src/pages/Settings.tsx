944b8987dd2aa89d0a871594e29b83186b2994b6
acarnevalli <49988241+acarnevalli@users.noreply.github.com>
2025-09-26 09:45:09 -0300

    Updated Settings.tsx

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Plus, Search, Edit2, Trash2, Shield, User, Mail, Clock, Settings as SettingsIcon, Users, LogOut } from 'lucide-react';
+import { Plus, Search, CreditCard as Edit2, Trash2, Shield, User, Mail, Clock, Settings as SettingsIcon, Users, LogOut } from 'lucide-react';
 import { useAuth, User as UserType } from '../contexts/AuthContext';
 import UserModal from '../components/UserModal';
 
